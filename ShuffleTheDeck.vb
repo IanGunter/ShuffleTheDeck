f475bdc6c4c5f8cdce17411c727aeef56bb4cf6b
Ian Gunter <iangunterofamerica@yahoo.com>
2020-09-20 12:36:10 -0600

    Add quit option

--- ShuffleTheDeck.vb
+++ ShuffleTheDeck.vb
@@ -2,7 +2,7 @@
 'RCET 0265
 'ShuffleTheDeck
 'Fall 2020
-'
+'https://github.com/IanGunter/ShuffleTheDeck.git
 
 
 Option Strict On
@@ -14,7 +14,6 @@ Module ShuffleTheDeck
     Sub Main()
         Dim row As Integer = 12
         Dim column As Integer = 3
-        Dim formattedstring As String
         Dim currentArray(row, column) As Boolean
         Dim randomColumn As Integer
         Dim randomRow As Integer
@@ -22,73 +21,93 @@ Module ShuffleTheDeck
         Dim suite As String
         Dim cardNumber As String
         Dim cardValue As String
+        Dim cardsDealt As Integer
         Randomize()
 
+        Console.WriteLine("Press enter to receive a card or type Q to quit. ")
+        If Console.ReadLine() = "Q" Then Exit Sub
+
         Do
+            If cardsDealt = 0 Then
 
-            randomColumn = CInt(Int((4 * Rnd())))
-            randomRow = CInt(Int((13 * Rnd())))
+            Else Console.WriteLine("Press enter to receive a card or type Q to quit. ")
+                If Console.ReadLine() = "Q" Then Exit Sub
+            End If
 
 
-            Do Until currentArray(randomRow, randomColumn) = False
+            cardsDealt += 1
+            Console.WriteLine(StrDup(4, vbTab) & "Cards Dealt: " & cardsDealt)
+
+
+            goodData = False
+            Do Until goodData = True
+                randomColumn = CInt(Int((4 * Rnd())))
+                randomRow = CInt(Int((13 * Rnd())))
+
                 If Not currentArray(randomRow, randomColumn) = True Then
                     currentArray(randomRow, randomColumn) = True
+                    goodData = True
 
-                Else
-                    Console.WriteLine($"Row{randomRow} Column{randomColumn}")
+                ElseIf goodData = False Then
+
                 End If
             Loop
 
 
             cardNumber = CStr(randomRow + 1)
-                If cardNumber = "11" Then
-                    cardValue = "Jack"
-                ElseIf cardNumber = "12" Then
-                    cardValue = "King"
-                ElseIf cardNumber = "13" Then
-                    cardValue = "Queen"
-                ElseIf cardNumber = "1" Then
-                    cardValue = "Ace"
-                Else cardValue = cardNumber
+            If cardNumber = "11" Then
+                cardValue = "Jack"
+            ElseIf cardNumber = "12" Then
+                cardValue = "King"
+            ElseIf cardNumber = "13" Then
+                cardValue = "Queen"
+            ElseIf cardNumber = "1" Then
+                cardValue = "Ace"
+            Else cardValue = cardNumber
 
-                End If
-
-
-                If randomColumn = 0 Then
-                    suite = "Spades"
-
-                ElseIf randomColumn = 1 Then
-                    suite = "Diamonds"
-
-                ElseIf randomColumn = 2 Then
-                    suite = "Hearts"
-
-                ElseIf randomColumn = 3 Then
-                    suite = "Clubs"
+            End If
 
 
-                End If
-                Console.WriteLine(cardValue & " Of " & suite)
-                Console.ReadLine()
+            If randomColumn = 0 Then
+                suite = "Spades"
+            ElseIf randomColumn = 1 Then
+                suite = "Diamonds"
+            ElseIf randomColumn = 2 Then
+                suite = "Hearts"
+            ElseIf randomColumn = 3 Then
+                suite = "Clubs"
+            End If
 
-            Loop
-        ' For i = 0 To row
-        'For j = 0 To column
 
+            Console.WriteLine(cardValue & " Of " & suite)
 
-        'formattedstring = $"{i},{j}"
-        'Console.Write(formattedstring.PadRight(8))
-        ' Console.Write(currentArray(row, column))
-        'If currentArray(i, j) Then
-        ' Console.Write(" T ")
-        ' Else
-        'Console.Write(" F ")
-        ' End If
-        'Next
+            If cardsDealt = 52 Then
+                Console.WriteLine(StrDup(4, vbTab) & "Cards Shuffled")
+                For row = 0 To 12
+                    For column = 0 To 3
+                        currentArray(row, column) = False
+                    Next
+                    cardsDealt = 0
+                Next
 
-        ' Console.WriteLine()
-        ' Next
+            Else
+            End If
+
+            If Console.ReadLine() = "Q" Then Exit Sub
+            Console.Clear()
+        Loop
 
     End Sub
 
 End Module
+
+
+
+
+
+
+
+
+
+
+
